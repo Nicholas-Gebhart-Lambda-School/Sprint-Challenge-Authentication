ae3d30c667f294fce73f4a35668d447f56c23bf0
gebhartn <nicholas.gebhart@gmail.com>
2019-11-15 08:10:22 -0800

    feat: add auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,18 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || "f2hjf92j223d3n";
+
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.decodedJwt = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
